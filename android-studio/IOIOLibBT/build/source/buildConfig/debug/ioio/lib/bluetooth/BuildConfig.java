59162092797a845c2a926966e48dc6f7cd4ce8a2
Jed Dawson <jeddwsn@gmail.com>
2014-03-31 21:59:14 -0700

    imported into Android Studio

---
+++ android-studio/IOIOLibBT/build/source/buildConfig/debug/ioio/lib/bluetooth/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ioio.lib.bluetooth;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
