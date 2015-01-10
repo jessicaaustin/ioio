465a051d0bf5d1e15d330280b700e53be81f6264
Jessica Austin <jessica@aus10.org>
2015-01-09 19:46:30 -0900

    remove build dir from source control

--- android-studio/IOIOLibBT/build/source/buildConfig/debug/ioio/lib/bluetooth/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package ioio.lib.bluetooth;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}
